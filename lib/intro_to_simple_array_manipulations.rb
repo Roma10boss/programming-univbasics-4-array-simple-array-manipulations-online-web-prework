8df13259d1205aee49f816b8fc1c9172ae09705d
Romario Gustave <romagustave10@gmail.com>
2019-10-08 23:03:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,11 @@
-def using_push(array,String)
+def using_push(array,dog)
 array.push('baby')
+end
+
+def using_unshift(array,people)
+array.unshift('boo')
+end
+
+def using_pop(array,cheese)
+  array.pop
 end
